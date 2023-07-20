2818cb8fcb356a09617f9b8d93b65969ccf075d0
Khushi Verma <khushiivrma@gmail.com>
2023-07-20 19:18:39 +0530

    Assignment2

---
+++ myapp/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  const [data, setData] = useState(0);
+
+  function getQuote(){
+    try {
+    fetch('https://api.quotable.io/random').then(
+      response => response.json()).then(
+        (quote)=>{
+          setData(quote);
+        }
+      )
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">        
+      <h1 className='Title'> Random Quote Generator</h1>
+      <h3 className='Quote'>Quote:{data.content}</h3> 
+      <h5 className='Author'>Author:{data.author}</h5> 
+      <button  className='Generator'onClick={getQuote}>Get Quote</button>               
+      </header>
+    </div>
+  );
+}
+
+export default App;
